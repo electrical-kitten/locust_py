e8d45bff46dcad0690a11cc343861daff4817490
borodinsky <borodinsky.v@gmail.com>
2025-04-08 14:24:29 +0300

    bla la la

--- locustfile.py
+++ locustfile.py
@@ -1,26 +1,62 @@
 from locust import HttpUser, TaskSet, task, between
 from req_check import user_info
+import random
 
-user_login = user_info['users'][0]['user_login']
-user_pass = user_info['users'][0]['user_password']
-token = user_info['users'][0]['token_shik']
-goods_id = 1787079
-project_id = 41740
+users = [
+    {
+    "login": user_info['users'][0]['user_login'], 
+     "password": user_info['users'][0]['user_password'], 
+     "token": user_info['users'][0]['token_shik']
+    },
+    {
+    "login": user_info['users'][1]['user_login'], 
+     "password": user_info['users'][1]['user_password'], 
+     "token": user_info['users'][1]['token_shik']
+    },
+    {
+    "login": user_info['users'][2]['user_login'], 
+    "password": user_info['users'][2]['user_password'], 
+    "token": user_info['users'][2]['token_shik']
+    }
+]
+
+goods_ids = [27, 25, 15, 6]
 
 class UserBehavior(HttpUser):
-    wait_time = between(1, 5)
+    wait_time = between(1, 10)
+
     def on_start(self):
-        self.client.post("/login/signin", json = {"login": user_login, "password": user_pass})
+        self.user_data = random.choice(users)
+        self.login()
 
-    @task(1)
+    def login(self):
+        response = self.client.post("/login/signin", json = {"login": self.user_data["login"], "password": self.user_data["password"]})
+        if response.status_code != 200:
+            self.environment.events.request.fire(
+                request_type="POST",
+                name = "login",
+                response_time = response.elapsed.total_seconds(),
+                response_length=len(response.content),
+                exception=Exception(f"Login failed: {response.text}"),
+            )
+
+
+    @task()
     def get_goods(self):
-        self.client.get(f"/api/v1/goods/{goods_id}", headers = {"FaceKitToken": token}) 
+        goods_id = random.choice(goods_ids)
+        response = self.client.get(f"/api/v1/goods/{goods_id}", headers = {"FaceKitToken": self.user_data["token"]})
+        if response.status_code != 200:
+            self.environment.events.request.fire(
+                request_type="GET",
+                name = "get_goods",
+                response_time = response.elapsed.total_seconds(),
+                response_length=len(response.content),
+                exception=Exception(f"Get goods failed: {response.text}"),
+            )
 
-    @task(2)
-    def get_project(self):
-        self.client.get(f"/api/v1/projects/{project_id}", headers = {"FaceKitToken": token})
 
-# class WebsiteUser(HttpUser):
-#     task_set = UserBehavior
-#     min_wait = 5000
-#     max_wait = 9000
+
+
+    # @task()
+    # def get_project(self):
+    #     self.client.get(f"/api/v1/projects/{project_id_ms}", headers = {"FaceKitToken": token})
--- req_check.py
+++ req_check.py
@@ -8,21 +8,42 @@ def user_info_conf():
 
 user_info = user_info_conf()
 
-user_login = user_info['users'][0]['user_login']
-user_pass = user_info['users'][0]['user_password']
+user_login = user_info['users'][1]['user_login']
+user_pass = user_info['users'][1]['user_password']
 base_url = "https://multishik.cynteka.ru"
-token = user_info['users'][0]['token_shik']
-# print(type(user_login))
-# print(type(user_pass))
+# base_url = "https://multivladick.cynteka.ru"
+token = user_info['users'][1]['token_shik']
+goods_id = 27
+# body = {
+#   "filters": [
+#     {
+#       "name": "шакал пик",
+#       "onlyWithNotBlockTransferState": "true",
+#       "typeId": "6",
+#       "grouped": "false",
+#       "sortBy": "id",
+#       "sortByOrder": "desc",
+#       "batchSize": 42,
+#       "excludedIds": []
+#     }
+#   ]
+# }
 
 sign_in = r.post(f"{base_url}/login/signin", json={"login": user_login, "password": user_pass})
 
-get_goods = r.get(f"{base_url}/api/v1/goods/1787079", headers = {"FaceKitToken": token})
+get_goods = r.get(f"{base_url}/api/v1/goods/{goods_id}", headers = {"FaceKitToken": token})
 
-get_project = r.get(f"{base_url}/api/v1/projects/41740", headers = {"FaceKitToken": token})
+# get_project = r.get(f"{base_url}/api/v1/projects/41740", headers = {"FaceKitToken": token})
+
+# filter_goods = r.post(f"{base_url}/api/v1/goods/filter", headers = {"FaceKitToken": token}, json = body)
+
+
 
 print(sign_in.status_code)
-# print(sign_in.text)
+print(sign_in.json())
 print(get_goods.status_code)
-print(get_project.status_code)
+# print(get_goods.json())
+# print(get_project.status_code)
 # print(get_project.text)
+# print(filter_goods.status_code)
+# print(filter_goods.json())
